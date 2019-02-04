81d0886c14ff74ce8c3c38da93b4658c417b0835
Aika W <aikawashington867@gmail.com>
2019-02-03 23:00:02 -0500

    added progam files

---
+++ ASCIIartProgram.java
@@ -0,0 +1,69 @@
+/*
+* 
+Title: ASCII Art Happy Umbrella
+@author: Aika Washington
+@since: 08/29/17
+@version: v1.0
+Description: This is a Java program that prints an ASCII Art picture of a happy umbrella when ran.
+Honor Code: On my honor, I have neither given onr received any unauthorized aid on this assignment. Aika Washington
+
+
+*/
+
+
+
+public class ASCIIartProgram
+
+{
+   public static void main(String[] args)
+  {
+  
+// \n first used here  
+System.out.println("\n");
+System.out.print("\n");
+// "\t" first used here; "\\ first used here"  
+System.out.println("\t\t\t\t\t\t//// /////////////////\\");
+System.out.println("\t\t\t\t\t////XXXXXXXXXXXXXXXXXXXXXXX\\\\");
+System.out.println("\t\t\t  ////  XXXX  X XXXX X X X XXX   XX\\\\\\");
+System.out.println("\t\t\t//X   XXXXXXXXXXXX X XXXXXXXXXXXXXXXXXX\\\\\\\\");
+System.out.println("\t\t  /X  XXXX    XXXXXXXXXXX         XX   XXXXXXX\\\\");
+System.out.println("\t\t //XXXX      XXX        XX          XX      X  XXX\\");
+//  \"  first used here
+System.out.println("\t\t/XXX  XXXXXXX        \"\"   X           X           \\\\\\");
+System.out.println("\t ///XXXXXX     XX        XXX   XX          X            \\");
+System.out.println("\t // XXXX   \"\"   XX       XXX    X          XX            \\");
+System.out.println("\t / XXX     XX     XX      XX     X          X            \\");
+System.out.println("\t/  XXX     XXX     X       X     XX         XX            \\");
+//  \'  first used here
+System.out.println("  / XXXX      XX      XX            X          XX\\\\\\\\\\\\\\\\   \\");
+System.out.println(" // XXXX       X       XX \'\'\'\'\'\'     X         XX\\       \\\\\\\\\\\\");
+System.out.println("/ XXXX X           \'\'\'\'\'\'\'    \'     X        \\\\\\           \\");
+System.out.println("/ XX X  X       \'\'\'\'         \'\'     \\\\\\\\    \\\\");
+System.out.println("/ XX X   XX      \'\'\'       \'\'\'  \\\\\\\\\\\\ \\\\\\\\\\\\");
+System.out.println("/ XX X    X        \'\'\'\'\'\'\'\'\'    \\\\  \\\\     \\\\    XX");
+System.out.println("/ XX       XX                   \\\\\\              XXX");
+System.out.println("/ XXXX      XX      /\\\\\\\\\\\\\\\\\\\\\\  X             XX XXX");
+System.out.println("/ XX X       XXX    //     XX XX  X             X    X");
+System.out.println("//XX           XXX  /       X     X             XXXXXX");
+System.out.println(" /XX             X///       X     XX");
+System.out.println(" //XXX    ///////////       XX     X");
+System.out.println("  //XXX  //                 XX     X");
+System.out.println("   /X XX /                   X     X");
+System.out.println("    // XX/       XXX         XX    X");
+System.out.println("\t  /////       XXXXXX      XX    X");
+System.out.println("\t  ////        X    XX     XX     X       XXXXX");
+System.out.println("                 X     X     XX     XX     XX   X");
+System.out.println("                 X     X     XX     XX     XX   X");
+System.out.println("                 XX    X     XXX    XXXXXXXX    X");
+System.out.println("                 XXXXXX      XX     X         XX");
+System.out.println("                              X X           XXX");
+System.out.println("                               XXXXXXXXXXXXX");
+System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t Aika Washington");
+System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        08.30.17");
+System.out.print("");
+System.out.print("");
+System.out.println("END RUN");
+
+      System.exit(0);
+  }
+}
---
+++ README.ASCIIartProgram.txt
@@ -0,0 +1,15 @@
+TITLE: Happy Umbrella
+AUTHOR/PROGRAMMER: Aika Washington
+DATE DUE: 08/30/17
+DATE SUBMITTED: 08/29/17
+COURSE TITLE: AP Computer Science
+MEETING TIME(S): 10:00am MWF
+DESCRIPTION: This is a Java program that prints an ASCII Art picture of a happy umbrella when ran.
+HONOR CODE: On my honor, I have neither given nor received any unauthorized aid on this assingment. Aika Washington
+HOWTO: Open the program with the IDE of your choice that supports Java. Compile then run the program.
+INPUT FILE: N/A
+OUTPUT FILE: N/A
+BIBLIOGRAPHY: asciiflow.com
+RESOURCES: N/A
+COMMENTS: N/A
+REFLECTION: I spread this project out over two days, I started the beginning of it one evening and finished the rest the next night. My biggest challenge was getting everything aligned while also having to use escape characters, so that caused a small need for adjustment, which I was able to overcome through constantly re-running the code to check and see if everything was aligned.
